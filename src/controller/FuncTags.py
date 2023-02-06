aa4f9dd24cae4c0a0b299385267734edd78fdbcd
QUICKLyas <2298930148@qq.com>
2023-02-06 21:21:39 +0800

    delete

---
+++ src/controller/FuncTags.py
@@ -0,0 +1,11 @@
+import time as t
+
+
+class Tags(object):
+    def __init__(self) -> None:
+        # self.tag =
+        pass
+
+    # 只存储tags，id是时间戳，所以需要updata的处理
+    def writeTagstoMongo(self):
+        return
---
+++ src/controller/FuncUser.py
@@ -0,0 +1,14 @@
+import time as t
+
+
+class Users(object):
+    def __init__(self) -> None:
+        # self.tag =
+        pass
+    # important things: 我们需要注意存入数据库中的数据
+    # 必需是有id和name字段的否则无法存储到数据库中
+    # 其次才是我们需要的简单信息
+    # 如果存储的是密码，应该考虑进行加密，或者权限设置
+
+    def writeUsertoMongo(self):
+        return
