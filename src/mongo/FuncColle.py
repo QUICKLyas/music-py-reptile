b985115224bda67da90d6291b79f1d9c17c32202
QUICKLyas <2298930148@qq.com>
2023-02-08 17:30:34 +0800

    20230208172954 start new

--- src/mongo/FuncColle.py
+++ src/mongo/FuncColle.py
@@ -1,11 +1,3 @@
-'''
-Author: QUICKLyas 2298930148@qq.com
-Date: 2023-01-18 19:44:58
-LastEditors: QUICKLyas 2298930148@qq.com
-LastEditTime: 2023-01-20 09:03:45
-FilePath: /reptile/src/mongo/FuncColle.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 import mongo.Connect as connect
 
 
