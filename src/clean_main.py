f9c43db808a00fdbd8960ca79432b64993a218ec
QUICKLyas <2298930148@qq.com>
2023-04-16 18:09:36 +0800

    clean_main.py complete

--- src/clean_main.py
+++ src/clean_main.py
@@ -18,46 +18,51 @@ f = fGJson.GetJson()
 # 首先连接基础表
 cols = connect.condb["song"]
 colsd = connect.condb["songdetail"]
-docs = cols.find({}, {}).limit(100).skip(1*100)
+
+i = 0
 # 循环体内部代码较多，提取出来，保证程序代码的整体平衡
-for item in docs:
-    # 获取id，根据id获取song和songdetail的信息，保存tag信息
-    id = item['id']
-    print(id)
-    # 获取歌曲信息
-    song = s.Song(id=id)
-    songDetail = s.SongDetail(ids=id)
-    songAble = s.SongAble(id=id)
-    # input()
-    context = f.getJsonFromUrl(song,  "song")
-    contextDetail = f.getJsonFromUrl(songDetail, "songdetail")
-    # check/music?id=可能出现问题，无法使用，延用之前保留的音乐权限记录
-    # contextAble = f.getCookieFromUrl(songAble, "songAble")
-    # print(song.getUrl())
-    # print("song:", context, "\n")
-    # print(songDetail.getUrl())
-    # print("songDetail:", contextDetail, "\n")
-    songs = contextDetail['songs'][0]
-    data = context['data'][0]
-    # print("songAble:",  contextAble, "\n")
-    print("id", item['id'], "\n")
-    # 更新
-    query = {"id": id}
-    new_values = {"$set": {
-        "songUrl": data['url'], "name": songs['name'], "mark": songs["mark"], "data": data}}
-    print(query, new_values)
-    # 对应于songDetail的信息更新程序
-    new_values_sd = {"$set": {
-        "name": songs["name"], "songs": songs}, "$unset": {"song": ""}}
-    print(query, new_values_sd)
-    pass
-    result = cols.update_one(query, new_values)
-    result_sd = colsd.update_one(query, new_values_sd)
-    # print(result.modified_count, result_sd.modified_count)
-    if(result.modified_count == 1 and result_sd.modified_count == 1):
-        print(result.modified_count, "complete", item['id'])
-    else:
-        print("no complete")
+while (True):
+
+    docs = cols.find({}, {}).limit(100).skip(i*100)
+    i += 1
+    for item in docs:
+        # 获取id，根据id获取song和songdetail的信息，保存tag信息
+        id = item['id']
+        print(id)
+        # 获取歌曲信息
+        song = s.Song(id=id)
+        songDetail = s.SongDetail(ids=id)
+        songAble = s.SongAble(id=id)
+        # input()
+        context = f.getJsonFromUrl(song,  "song")
+        contextDetail = f.getJsonFromUrl(songDetail, "songdetail")
+        # check/music?id=可能出现问题，无法使用，延用之前保留的音乐权限记录
+        # contextAble = f.getCookieFromUrl(songAble, "songAble")
+        # print(song.getUrl())
+        # print("song:", context, "\n")
+        # print(songDetail.getUrl())
+        # print("songDetail:", contextDetail, "\n")
+        songs = contextDetail['songs'][0]
+        data = context['data'][0]
+        # print("songAble:",  contextAble, "\n")
+        # print("id", item['id'], "\n")
+        # 更新
+        query = {"id": id}
+        new_values = {"$set": {
+            "songUrl": data['url'], "name": songs['name'], "mark": songs["mark"], "data": data}}
+        # print(query, new_values)
+        # 对应于songDetail的信息更新程序
+        new_values_sd = {"$set": {
+            "name": songs["name"], "songs": songs}, "$unset": {"song": ""}}
+        # print(query, new_values_sd)
+        pass
+        result = cols.update_one(query, new_values)
+        result_sd = colsd.update_one(query, new_values_sd)
+        # print(result.modified_count, result_sd.modified_count)
+        if(result.modified_count == 1 and result_sd.modified_count == 1):
+            print(result.modified_count, "complete", item['id'])
+        else:
+            print("no complete")
 
 
 # 更新数据到原表中，保证只改变需要改变的数据
